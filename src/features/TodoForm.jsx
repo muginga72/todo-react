2a9bd6d1d56f542f38e51eedb815527197786106
muginga72 <lmj.muginga@gmail.com>
2025-08-13 12:43:49 -0400

    Week-11 Advanced State and useReducer, dispatches

--- src/features/TodoForm.jsx
+++ src/features/TodoForm.jsx
@@ -1,14 +1,17 @@
 import React, { useState, useRef } from 'react';
 import TextInputWithLabel from '../shared/TextInputWithLabel';
 
+// Retained local state for input, passed new todo to parent via onAddTodo
 function TodoForm({ onAddTodo }) {
   const [workingTodoTitle, setWorkingTodoTitle] = useState('');
   const inputRef = useRef(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onAddTodo(workingTodoTitle);
-    setWorkingTodoTitle('');
+    if (workingTodoTitle.trim()) {
+      onAddTodo(workingTodoTitle.trim());
+      setWorkingTodoTitle('');
+    }
   };
 
   return (
